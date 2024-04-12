c364d81c5543c10e1213029abbef1973cd054833
Frank Albarracín <molina.frank@gmail.com>
2024-04-11 22:38:17 -0400

    cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from 'astro/config';
-
 import sitemap from '@astrojs/sitemap';
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://myhealthfair.com',
-	integrations: [sitemap()],
-	prefetch: true,
-	format: 'file',
-	output: 'hybrid'
+  site: 'https://myhealthfair.com',
+  integrations: [sitemap()],
+  prefetch: true,
+  format: 'file',
+  output: 'hybrid',
+  adapter: cloudflare()
 });
