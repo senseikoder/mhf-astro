51e5b8fac8aad0cda877edcf7244e38b23597d50
Frank Albarracín <molina.frank@gmail.com>
2024-06-26 11:58:24 -0400

    fix api

--- src/pages/api/sendEmail.json.ts
+++ src/pages/api/sendEmail.json.ts
@@ -1,11 +1,11 @@
 export const prerender = false;
 import type { APIRoute } from "astro";
-import { Resend } from "resend";
+import { Resend } from "node:resend";
 
 const resend = new Resend(import.meta.env.RESEND_API_KEY);
 
 export const POST: APIRoute = async ({ request }) => {
-   try {
+   try {     
       const body = await request.json();
 
       const { to, from, html, subject } = body;
