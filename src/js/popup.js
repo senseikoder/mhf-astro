57ec2a4ec4013c89b71ebba44534e1d420f74b35
Frank Albarracín <molina.frank@gmail.com>
2024-07-27 00:09:36 -0400

    fixes before run

---
+++ src/js/popup.js
@@ -0,0 +1,48 @@
+
+document.addEventListener("astro:page-load", () => {
+
+   // Functions to open and close a modal
+   function openModal($el) {
+      $el.classList.add("is-active");
+   }
+
+   function closeModal($el) {
+      $el.classList.remove("is-active");
+   }
+
+   function closeAllModals() {
+      (document.querySelectorAll(".modal") || []).forEach(($modal) => {
+         closeModal($modal);
+      });
+   }
+
+
+   // Add a click event on various child elements to close the parent modal
+   (
+      document.querySelectorAll(
+         ".modal-background, .modal-close, .delete",
+      ) || []
+   ).forEach(($close) => {
+      const $target = $close.closest(".modal");
+
+      $close.addEventListener("click", () => {
+         closeModal($target);
+      });
+   });
+
+   // Add a keyboard event to close all modals
+   document.addEventListener("keydown", (event) => {
+      if (event.key === "Escape") {
+         closeAllModals();
+      }
+   });
+
+   // carga Modal
+
+   setTimeout(() => {
+      const $target = document.getElementById('modal-popup')
+      // openModal($target)
+
+   }, 4000);
+
+});
