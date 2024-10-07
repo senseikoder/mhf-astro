73aa82f0438926f22e22a6440efd0d51863972c2
Frank Albarracín <molina.frank@gmail.com>
2024-10-07 11:20:19 -0400

    back to netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
-// import netlify from "@astrojs/netlify";
+import netlify from "@astrojs/netlify";
 
-import vercel from "@astrojs/vercel/serverless";
+// import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
@@ -15,7 +15,7 @@ export default defineConfig({
     defaultLocale: "es",
     locales: ["es", "en"]
   },
-  adapter: vercel(),
+  adapter: netlify(),
   redirects: {
     '/blog': '/blog/page',
     '/en/blog': '/en/blog/page'
