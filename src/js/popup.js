8cb009d26c987f9eed6522d53eea5d4049d1417b
Frank Albarracín <molina.frank@gmail.com>
2025-07-29 13:56:38 -0400

    custom chatbot

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,81 +1,56 @@
 document.addEventListener("astro:page-load", () => {
-  // Función para establecer una cookie con un valor y un tiempo de expiración en días
-  function setCookie(name, value, days) {
-    const date = new Date();
-    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
-    const expires = "expires=" + date.toUTCString();
-    document.cookie = name + "=" + value + ";" + expires + ";path=/";
-  }
-
-  // Función para obtener el valor de una cookie
-  function getCookie(name) {
-    const nameEQ = name + "=";
-    const ca = document.cookie.split(";");
-    for (let i = 0; i < ca.length; i++) {
-      let c = ca[i];
-      while (c.charAt(0) === " ") c = c.substring(1, c.length);
-      if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
-    }
-    return null;
-  }
-
-  // Functions to open and close a modal
-  function openModal($el) {
-    $el.classList.add("is-active");
-  }
-
-  function closeModal($el) {
-    $el.classList.remove("is-active");
-    setCookie("modalClosed", "true", 1); // 1 día
-  }
-
-  function closeAllModals() {
-    (document.querySelectorAll(".modal") || []).forEach(($modal) => {
-      closeModal($modal);
-    });
-  }
-
-  // Add a click event on various child elements to close the parent modal
-  (
-    document.querySelectorAll(".modal-background, .modal-close, .delete") || []
-  ).forEach(($close) => {
-    const $target = $close.closest(".modal");
-
-    $close.addEventListener("click", () => {
-      closeModal($target);
-    });
-  });
-
-  // Add a keyboard event to close all modals
-  document.addEventListener("keydown", (event) => {
-    if (event.key === "Escape") {
-      closeAllModals();
-    }
-  });
-
-  // Check if the modal should be displayed
-  if (!getCookie("modalClosed")) {
-    // Load Modal after 4 seconds
-    setTimeout(() => {
-      const $target = document.getElementById("modal-popup");
-      // openModal($target);
-    }, 7000);
-  }
-
-  /////////////////////////
   //   chatbot OpenWidget   //
 
-//   const chatbot = document.getElementById("chatbot");
-
-//   chatbot.addEventListener("click", () => {
-//     OpenWidget.call("maximize");
-//   });
-
-  function onReady() {
-    if (!("ontouchstart" in window)) {
-      OpenWidget.call("maximize");
+  window.__ow = window.__ow || {};
+  window.__ow.organizationId = "3e061902-7c69-480f-8b82-ae0a21f6b0e8";
+  window.__ow.asyncInit = true;
+  (function (n, t, c) {
+    function i(n) {
+      return e._h ? e._h.apply(null, n) : e._q.push(n);
     }
-  }
+    var e = {
+      _q: [],
+      _h: null,
+      _v: "2.0",
+      on: function () {
+        i(["on", c.call(arguments)]);
+      },
+      once: function () {
+        i(["once", c.call(arguments)]);
+      },
+      off: function () {
+        i(["off", c.call(arguments)]);
+      },
+      get: function () {
+        if (!e._h)
+          throw new Error("[OpenWidget] You can't use getters before load.");
+        return i(["get", c.call(arguments)]);
+      },
+      call: function () {
+        i(["call", c.call(arguments)]);
+      },
+      init: function () {
+        var n = t.createElement("script");
+        (n.async = !0),
+          (n.type = "text/javascript"),
+          (n.src = "https://cdn.openwidget.com/openwidget.js"),
+          t.head.appendChild(n);
+      },
+    };
+    !n.__ow.asyncInit && e.init(), (n.OpenWidget = n.OpenWidget || e);
+  })(window, document, [].slice);
 
-  OpenWidget.on("ready", onReady);
+  const chatbot = document.getElementById("chatbot");
+
+  chatbot.addEventListener("click", () => {
+    console.log("click");
+    OpenWidget.init();
+});
+
+
+setTimeout(() => {
+   console.log("init");
+   console.log(OpenWidget);
+   OpenWidget.init();
+  }, 4000);
 });
