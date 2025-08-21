a9075c0f4d44d504edb633816b9c93da7e51665e
Frank Albarracín <molina.frank@gmail.com>
2025-08-21 18:59:38 -0400

    -host

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,21 +5,36 @@ import vue from "@astrojs/vue";
 import icon from "astro-icon";
 import path from "path";
 
+import purgecss from "astro-purgecss";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://myhealthfair.com",
   server: {
-    host: true,
     port: Number(process.env.PORT) || 4323,
   },
-  integrations: [sitemap({
-    customPages: [
-      "https://myhealthfair.com/booths/alzheimer-assoc/",
-      "https://myhealthfair.com/en/booths/alzheimer-assoc/",
-      "https://myhealthfair.com/booths/united-health-care/",
-      "https://myhealthfair.com/en/booths/united-health-care/",
-    ],
-  }), vue(), icon()],
+  integrations: [
+    sitemap({
+      customPages: [
+        "https://myhealthfair.com/booths/alzheimer-assoc/",
+        "https://myhealthfair.com/en/booths/alzheimer-assoc/",
+        "https://myhealthfair.com/booths/united-health-care/",
+        "https://myhealthfair.com/en/booths/united-health-care/",
+      ],
+    }),
+    vue(),
+    icon(),
+    purgecss({
+      content: ["./src/**/*.{astro,html,js,vue}"],
+      extractors: [
+        {
+          extractor: (content) => content.match(/[\w-/:]+(?<!:)/g) ?? [],
+          extensions: ["astro", "html", "vue", "js"],
+        },
+      ],
+      
+    }),
+  ],
   prefetch: true,
   format: "file",
   output: "static",
